3c02a4acaa5e1f47a0a9dbefca17675a220696ca
Michael Okolo <michael.okolo@beopen.com>
2023-11-09 18:47:38 +1100

    Changed nvim theme back to catpuccin

--- nvim/custom/chadrc.lua
+++ nvim/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "dark_horizon",
-  theme_toggle = { "dark_horizon", "one_light" },
+  theme = "catppuccin",
+  theme_toggle = { "catppuccin", "one_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
