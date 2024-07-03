dd06815f5b38cda38ebfbe0d079ef8a4692e5564
Michael Okolo <okolomikel2009@gmail.com>
2024-07-04 00:16:33 +1000

    Added obisidian nvim to my nvim plugin

--- nvim/custom/plugins.lua
+++ nvim/custom/plugins.lua
@@ -130,7 +130,7 @@ local plugins = {
   -- },
   {
     "github/copilot.vim",
-    event = "BufEnter"
+    event = "BufEnter",
   },
   {
     "michaelrommel/nvim-silicon",
@@ -151,8 +151,41 @@ local plugins = {
           return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
         end,
       })
-    end
-  }
+    end,
+  },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    --   "BufReadPre path/to/my-vault/**.md",
+    --   "BufNewFile path/to/my-vault/**.md",
+    -- },
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+
+      -- see below for full list of optional dependencies 👇
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "Obsidian Notes",
+          path = "~/Documents/Obsidian/",
+        },
+        -- {
+        -- 	name = "work",
+        -- 	path = "~/vaults/work",
+        -- },
+      },
+
+      -- see below for full list of options 👇
+    },
+  },
 }
 
 return plugins
