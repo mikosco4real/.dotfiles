07c33e49d92845d8c81c0483a281c6884bb99cb8
Michael Okolo <okolomikel2009@gmail.com>
2024-08-14 00:35:05 +1000

    Updated my nvim plugins

--- nvim/custom/plugins.lua
+++ nvim/custom/plugins.lua
@@ -1,191 +1,197 @@
 local overides = require("custom.configs.overrides")
 local plugins = {
-	{
-		"williamboman/mason.nvim",
-		opts = overides.mason,
-	},
-	{
-		"mfussenegger/nvim-dap",
-		config = function(_, opts)
-			require("core.utils").load_mappings("dap")
-		end,
-	},
-	{
-		"leoluz/nvim-dap-go",
-		ft = "go",
-		dependencies = "mfussenegger/nvim-dap",
-		config = function(_, opts)
-			require("dap-go").setup(opts)
-			--    require("core.utils").load_mappings("dap_go")
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			-- Formatting and linting
-			{
-				"jose-elias-alvarez/null-ls.nvim",
-				config = function()
-					require("custom.configs.null-ls")
-				end,
-			},
-		},
-		config = function()
-			require("plugins.configs.lspconfig")
-			require("custom.configs.lspconfig")
-		end,
-	},
-	{
-		"olexsmir/gopher.nvim",
-		ft = "go",
-		config = function(_, opts)
-			require("gopher").setup(opts)
-			require("core.utils").load_mappings("gopher")
-		end,
-		build = function()
-			vim.cmd([[silent! GoInstallDeps]])
-		end,
-	},
-	{
-		"nvim-telescope/telescope-fzf-native.nvim",
-		run = "make",
-	},
-	-- Add Lazygit integration
-	{
-		"kdheepak/lazygit.nvim",
-		dependencies = {
-			"nvim-telescope/telescope.nvim",
-			"nvim-lua/plenary.nvim",
-		},
-		config = function()
-			require("telescope").load_extension("lazygit")
-		end,
-	},
-	{
-		"max397574/better-escape.nvim",
-		event = "InsertEnter",
-		config = function()
-			require("better_escape").setup()
-		end,
-	},
-	{
-		"rcarriga/nvim-dap-ui",
-		dependencies = "mfussenegger/nvim-dap",
-		config = function()
-			local dap = require("dap")
-			local dapui = require("dapui")
-			dapui.setup()
-			dap.listeners.after.event_initialized["dapui_config"] = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated["dapui_config"] = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited["dapui_config"] = function()
-				dapui.close()
-			end
-		end,
-	},
-	{
-		"mfussenegger/nvim-dap",
-		ft = "python",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-			"rcarriga/nvim-dap-ui",
-		},
-		config = function(_, opts)
-			-- local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-			-- require("dap-python").setup(path)
-			-- require("core.utils").load_mappings "dap_python"
-		end,
-	},
-	{
-		"rust-lang/rust.vim",
-		ft = "rust",
-		init = function()
-			vim.g.rustfmt_autosave = 1
-		end,
-	},
-	{
-		"saecki/crates.nvim",
-		dependencies = "hrsh7th/nvim-cmp",
-		ft = { "rust", "toml" },
-		config = function(_, opts)
-			local crates = require("crates")
-			crates.setup(opts)
-			crates.show()
-		end,
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		opts = function()
-			local M = require("plugins.configs.cmp")
-			table.insert(M.sources, { name = "crates" })
-			return M
-		end,
-	},
-	-- {
-	--   "Exafunction/codeium.vim",
-	--   event = "BufEnter",
-	-- },
-	-- {
-	--   "github/copilot.vim",
-	--   event = "BufEnter",
-	-- },
-	{
-		"michaelrommel/nvim-silicon",
-		lazy = true,
-		cmd = "Silicon",
-		init = function()
-			local wk = require("which-key")
-			wk.register({
-				["<leader>sc"] = { ":Silicon<CR>", "Snapshot Code" },
-			}, { mode = "v" })
-		end,
-		config = function()
-			require("silicon").setup({
-				font = "JetBrainsMono Nerd Font=34; Noto Color Emoji=34",
-				theme = "Dracula",
-				background = "#94e2d5",
-				window_title = function()
-					return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
-				end,
-			})
-		end,
-	},
-	{
-		"epwalsh/obsidian.nvim",
-		version = "*", -- recommended, use latest release instead of latest commit
-		lazy = true,
-		ft = "markdown",
-		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-		-- event = {
-		--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-		--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-		--   "BufReadPre path/to/my-vault/**.md",
-		--   "BufNewFile path/to/my-vault/**.md",
-		-- },
-		dependencies = {
-			-- Required.
-			"nvim-lua/plenary.nvim",
+  {
+    "williamboman/mason.nvim",
+    opts = overides.mason,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("core.utils").load_mappings("dap")
+    end,
+  },
+  {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("dap-go").setup(opts)
+      --    require("core.utils").load_mappings("dap_go")
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- Formatting and linting
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require("custom.configs.null-ls")
+        end,
+      },
+    },
+    config = function()
+      require("plugins.configs.lspconfig")
+      require("custom.configs.lspconfig")
+    end,
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+      require("core.utils").load_mappings("gopher")
+    end,
+    build = function()
+      vim.cmd([[silent! GoInstallDeps]])
+    end,
+  },
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    run = "make",
+  },
+  -- Add Lazygit integration
+  {
+    "kdheepak/lazygit.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("telescope").load_extension("lazygit")
+    end,
+  },
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function(_, opts)
+      -- local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+      -- require("dap-python").setup(path)
+      -- require("core.utils").load_mappings "dap_python"
+    end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+  {
+    "saecki/crates.nvim",
+    dependencies = "hrsh7th/nvim-cmp",
+    ft = { "rust", "toml" },
+    config = function(_, opts)
+      local crates = require("crates")
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local M = require("plugins.configs.cmp")
+      table.insert(M.sources, { name = "crates" })
+      return M
+    end,
+  },
+  -- {
+  --   "Exafunction/codeium.vim",
+  --   event = "BufEnter",
+  -- },
+  -- {
+  --   "github/copilot.vim",
+  --   event = "BufEnter",
+  -- },
+  {
+    "michaelrommel/nvim-silicon",
+    lazy = true,
+    cmd = "Silicon",
+    init = function()
+      local wk = require("which-key")
+      wk.add({
+        { "<leader>sc", ":Silicon<CR>", desc = "Snapshot Code", mode = "v" },
+      })
+    end,
+    config = function()
+      require("silicon").setup({
+        font = "JetBrainsMono Nerd Font=34; Noto Color Emoji=34",
+        theme = "Dracula",
+        background = "#94e2d5",
+        window_title = function()
+          return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
+        end,
+      })
+    end,
+  },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    --   "BufReadPre path/to/my-vault/**.md",
+    --   "BufNewFile path/to/my-vault/**.md",
+    -- },
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
 
-			-- see below for full list of optional dependencies 👇
-		},
-		opts = {
-			workspaces = {
-				{
-					name = "Obsidian Notes",
-					path = "~/Documents/Obsidian/",
-				},
-				-- {
-				-- 	name = "work",
-				-- 	path = "~/vaults/work",
-				-- },
-			},
+      -- see below for full list of optional dependencies 👇
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "Obsidian Notes",
+          path = "~/Documents/Obsidian/",
+        },
+        -- {
+        -- 	name = "work",
+        -- 	path = "~/vaults/work",
+        -- },
+      },
 
-			-- see below for full list of options 👇
-		},
-	},
+      -- see below for full list of options 👇
+    },
+  },
+  { "echasnovski/mini.icons", version = false },
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
+    config = true,
+  },
 }
 
 return plugins
