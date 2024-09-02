b81c26621795ae90d66b79296225e3c449023a55
Michael Okolo <okolomikel2009@gmail.com>
2024-09-03 07:30:50 +1000

    Added rustacean nvim as a plugin for rust to replace rust-analyser from MasonInstall

--- nvim/custom/configs/lspconfig.lua
+++ nvim/custom/configs/lspconfig.lua
@@ -5,80 +5,65 @@ local lspconfig = require("lspconfig")
 local util = require("lspconfig/util")
 
 local common_servers = {
-	"tailwindcss",
-	"cssls",
-	"jsonls",
-	"tsserver",
-	"sqlls",
-	"dockerls",
-	"marksman",
-	"docker_compose_language_service",
-	"angularls",
+  "tailwindcss",
+  "cssls",
+  "jsonls",
+  "tsserver",
+  "sqlls",
+  "dockerls",
+  "marksman",
+  "docker_compose_language_service",
+  "angularls",
 }
 
 for _, lsp in ipairs(common_servers) do
-	lspconfig[lsp].setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-	})
+  lspconfig[lsp].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+  })
 end
 
 lspconfig.gopls.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	cmd = { "gopls" },
-	filetypes = { "go", "gomod", "gowork", "gotmpl" },
-	root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-	settings = {
-		gopls = {
-			completeUnimported = true,
-			usePlaceholders = true,
-			analyses = {
-				unusedparams = true,
-			},
-		},
-	},
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+      },
+    },
+  },
 })
 
 -- Python setup
 lspconfig.pyright.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-})
-
--- Rust setup
-lspconfig.rust_analyzer.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	filetypes = { "rust" },
-	root_dir = util.root_pattern("Cargo.toml"),
-	settings = {
-		["rust-analyzer"] = {
-			cargo = {
-				allFeatures = true,
-			},
-		},
-	},
+  on_attach = on_attach,
+  capabilities = capabilities,
 })
 
 -- SQL setup
 lspconfig.sqlls.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	filetypes = { "sql", "psql", "mysql" },
-	root_dir = util.root_pattern(".git", ".env"),
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "sql", "psql", "mysql" },
+  root_dir = util.root_pattern(".git", ".env"),
 })
 
 -- Configure Lua
 lspconfig.lua_ls.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	autostart = true,
-	settings = {
-		Lua = {
-			diagnostics = { globals = { "vim" } },
-		},
-	},
+  on_attach = on_attach,
+  capabilities = capabilities,
+  autostart = true,
+  settings = {
+    Lua = {
+      diagnostics = { globals = { "vim" } },
+    },
+  },
 })
 
 -- Configure html
@@ -86,34 +71,34 @@ local cp = vim.lsp.protocol.make_client_capabilities()
 cp.textDocument.completion.completionItem.snippetSupport = true
 
 lspconfig.html.setup({
-	on_attach = on_attach,
-	capabilities = cp,
-	cmd = { "vscode-html-language-server", "--stdio" },
-	filetypes = { "html" },
-	init_options = {
-		configurationSection = { "html", "css", "javascript" },
-		embeddedLanguages = {
-			css = true,
-			javascript = true,
-		},
-		provideFormatter = true,
-	},
-	root_dir = util.root_pattern(".git", ".env", "*.html"),
-	settings = {},
-	single_file_support = true,
-	opts = {
-		settings = {
-			html = {
-				format = {
-					templating = true,
-					wrapLineLength = 120,
-					wrapAttributes = "auto",
-				},
-				hover = {
-					documention = true,
-					references = true,
-				},
-			},
-		},
-	},
+  on_attach = on_attach,
+  capabilities = cp,
+  cmd = { "vscode-html-language-server", "--stdio" },
+  filetypes = { "html" },
+  init_options = {
+    configurationSection = { "html", "css", "javascript" },
+    embeddedLanguages = {
+      css = true,
+      javascript = true,
+    },
+    provideFormatter = true,
+  },
+  root_dir = util.root_pattern(".git", ".env", "*.html"),
+  settings = {},
+  single_file_support = true,
+  opts = {
+    settings = {
+      html = {
+        format = {
+          templating = true,
+          wrapLineLength = 120,
+          wrapAttributes = "auto",
+        },
+        hover = {
+          documention = true,
+          references = true,
+        },
+      },
+    },
+  },
 })
--- nvim/custom/configs/overrides.lua
+++ nvim/custom/configs/overrides.lua
@@ -1,103 +1,103 @@
 local M = {}
 
 M.treesitter = {
-	ensure_installed = {
-		"vim",
-		"lua",
-		"markdown",
-		"markdown_inline",
-		"go",
-		"python",
-		"html",
-		"css",
-		"javascript",
-		"sql",
-		"docker",
-		"yaml",
-		"json",
-		"c",
-		"typescript",
-		"tsx",
-		"latex",
-		"make",
-		"dockerfile",
-		"gitignore",
-		"tailwindcss",
-		"rust",
-	},
-	indent = {
-		enable = true,
-		-- disable = {
-		--   "python"
-		-- },
-	},
+  ensure_installed = {
+    "vim",
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "go",
+    "python",
+    "html",
+    "css",
+    "javascript",
+    "sql",
+    "docker",
+    "yaml",
+    "json",
+    "c",
+    "typescript",
+    "tsx",
+    "latex",
+    "make",
+    "dockerfile",
+    "gitignore",
+    "tailwindcss",
+    "rust",
+    "toml",
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
 }
 
 M.mason = {
-	ensure_installed = {
-		-- lua stuff
-		"lua-language-server",
-		"stylua",
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
 
-		-- python stuff
-		"pyright",
-		"flake8", -- linter: other popular linters - mypy, yapf, ruff
-		"black",
-		"isort",
-		"debugpy",
+    -- python stuff
+    "pyright",
+    "flake8", -- linter: other popular linters - mypy, yapf, ruff
+    "black",
+    "isort",
+    "debugpy",
 
-		-- go stuff
-		"gopls",
-		"gofumpt",
-		"golines",
+    -- go stuff
+    "gopls",
+    "gofumpt",
+    "golines",
 
-		-- JSON stuff
-		"json-lsp",
+    -- JSON stuff
+    "json-lsp",
 
-		-- YAML stuff
-		"yaml-language-server",
+    -- YAML stuff
+    "yaml-language-server",
 
-		-- Web stuff
-		"html-lsp",
-		"css-lsp",
-		"typescript-language-server",
-		"angular-language-server",
-		"eslint_d",
-		"tailwindcss-language-server",
+    -- Web stuff
+    "html-lsp",
+    "css-lsp",
+    "typescript-language-server",
+    "angular-language-server",
+    "eslint_d",
+    "tailwindcss-language-server",
 
-		-- SQL stuff
-		"sqlls",
+    -- SQL stuff
+    "sqlls",
 
-		-- Markdown
-		"marksman",
+    -- Markdown
+    "marksman",
 
-		-- Docker stuff
-		"docker-compose-language-service",
-		"dockerfile-language-server",
+    -- Docker stuff
+    "docker-compose-language-service",
+    "dockerfile-language-server",
 
-		-- LaTeX
-		"ltex-ls",
+    -- LaTeX
+    "ltex-ls",
 
-		-- Rust
-		"rust-analyzer",
-		"rustfmt",
-	},
+    -- Rust
+    "rustfmt",
+  },
 }
 
 -- git support in nvimtree
 M.nvimtree = {
-	git = {
-		enable = true,
-	},
+  git = {
+    enable = true,
+  },
 
-	renderer = {
-		highlight_git = true,
-		icons = {
-			show = {
-				git = true,
-			},
-		},
-	},
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
 }
 
 return M
