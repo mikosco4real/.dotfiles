2e43b7b3ea3badef784225565e1c521051a1c84f
Michael Okolo <mokolo@Michaels-MacBook-Pro.local>
2024-10-08 23:43:19 +1100

    Updated nvim config

---
+++ nvim/nvim/lua/configs/treesitter.lua
@@ -0,0 +1,29 @@
+local opts = require "nvchad.configs.treesitter"
+
+opts.ensure_installed = {
+    "lua",
+    "luadoc",
+    "vim",
+    "vimdoc",
+    "python",
+    "rust",
+    "go",
+    "printf",
+    "javascript",
+    "bash",
+    "css",
+    "json",
+    "html",
+    "markdown",
+    "markdown_inline",
+    "nginx",
+    "php",
+    "sql",
+    "tmux",
+    "tsx",
+    "typescript",
+    "vue",
+    "yaml",
+}
+
+return opts
