080c147b05c09a54f395a72c80c7e3fb612d92d3
Michael Okolo <mokolo@Michaels-MacBook-Pro.local>
2025-10-17 17:49:55 +1100

    Updated lspconfig

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,9 +1,6 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
--- EXAMPLE
 local servers = {
     "html",
     "cssls",
@@ -22,28 +19,7 @@ local servers = {
     "docker_compose_language_service",
     "rust_analyzer",
 }
-local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-    lspconfig[lsp].setup {
-        on_attach = nvlsp.on_attach,
-        on_init = nvlsp.on_init,
-        capabilities = nvlsp.capabilities,
-    }
-end
+vim.lsp.enable(servers)
 
--- configuring single server, example: typescript
-lspconfig.ts_ls.setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-}
-
--- configure sqls
--- lspconfig.sqls.setup {
---     on_attach = nvlsp.on_attach,
---     on_init = nvlsp.on_init,
---     capabilities = nvlsp.capabilities,
---     single_file_support = true,
--- }
+-- to configure lsps further read :h vim.lsp.config
